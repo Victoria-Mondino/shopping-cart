c12cb4a108232589ac11ac57580da5f3cd01698d
Victoria Mondino <vicki.mondino@hotmail.com>
2024-10-21 15:22:08 -0300

    actualizacion proyecto final

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,38 @@
+let productos = [];
+
+// Cargar los productos desde el JSON
+fetch("./src/js/productos.json")
+    .then(response => response.json())
+    .then(data => {
+        productos = data;
+        cargarProductos(productos);
+    });
+
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
 
-// Función para cargar productos
+// Filtrar y cargar productos por categoría
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
+        }
+    });
+});
+
+// Cargar los productos al HTML
 function cargarProductos(productosElegidos) {
     contenedorProductos.innerHTML = "";
 
@@ -12,37 +43,80 @@ function cargarProductos(productosElegidos) {
             <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
             <div class="producto-detalles">
                 <h3 class="producto-titulo">${producto.titulo}</h3>
-                <p class="producto-precio">${producto.precio}</p>
-                <p class="producto-descripcion">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p class="producto-precio">$${producto.precio}</p>
                 <button class="producto-agregar" id="${producto.id}">Agregar</button>
             </div>
         `;
-        contenedorProductos.appendChild(div);
+
+        contenedorProductos.append(div);
+    });
+
+    actualizarBotonesAgregar();
+}
+
+// Actualizar botones de agregar productos
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
     });
 }
 
-// Función para manejar los botones de categorías
-function agregarEventoCategorias(productos) {
-    botonesCategorias.forEach(boton => {
-        boton.addEventListener("click", (e) => {
-            botonesCategorias.forEach(boton => boton.classList.remove("active"));
-            e.currentTarget.classList.add("active");
+let productosEnCarrito = [];
 
-            if (e.currentTarget.id != "todos") {
-                const productosFiltrados = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
-                cargarProductos(productosFiltrados);
-            } else {
-                cargarProductos(productos);
-            }
-        });
-    });
+// Cargar productos del carrito desde localStorage
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito(); // Actualizar número de productos
+} else {
+    productosEnCarrito = [];
 }
 
-// Llamada a fetch para cargar productos desde el archivo JSON
-fetch('./public/productos.json')
-    .then(response => response.json())
-    .then(data => {
-        cargarProductos(data.productos); // Cargar productos
-        agregarEventoCategorias(data.productos); // Agregar eventos a los botones de categoría
-    })
-    .catch(error => console.error('Error al cargar los productos:', error));
+// Agregar productos al carrito
+function agregarAlCarrito(e) {
+    Toastify({
+        text: "Producto agregado",
+        duration: 3000,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+            background: "linear-gradient(to right, #4b33a8, #785ce9)",
+            borderRadius: "2rem",
+            textTransform: "uppercase",
+            fontSize: ".75rem"
+        },
+        offset: {
+            x: '1.5rem',
+            y: '1.5rem'
+        },
+        onClick: function() {}
+    }).showToast();
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if (productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    actualizarNumerito();
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+    
+    // Actualizar carrito visualmente
+    cargarProductosCarrito();
+}
+
+// Actualizar número de productos en el carrito
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
